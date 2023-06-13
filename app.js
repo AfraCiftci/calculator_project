621574c42ec2488c5c91856b080e4785d0a945ea
Afra <nurafraciftci@gmail.com>
2023-06-13 15:13:16 +0200

    v2

--- app.js
+++ app.js
@@ -3,3 +3,43 @@ const current = document.querySelector(".current-display");
 const buttons = document.querySelector(".buttons-container");
 
 
+//? THERE ARE GLOBAL
+let altSatir = "";
+let ustSatir = "";
+let operator = "";
+
+buttons.addEventListener("click", (e) => {
+    if (e.target.classList.contains("num")) {
+        sayiyiAl(e.target.textContent);
+        yazdirilanAlan();
+}
+
+if (e.target.classList.contains("operator")) {
+    choose(e.target.textContent);
+    yazdirilanAlan();
+}
+
+if (e.target.classList.contains("equal")) {
+    hesapla();
+    yazdirilanAlan();
+}
+
+if (e.target.classList.contains("pm")) {
+    if (!altSatir) return;
+    altSatir /= 100;
+    yazdirilanAlan();
+} 
+
+if (e.target.classList.contains("percent")) {
+    if (!altSatir) return;
+    altSatir /= 100;
+    yazdirilanAlan();
+}
+
+if (e.target.classList.contains("ac")) {
+    altSatir = "";
+    ustSatir = "";
+    operator = "";
+    yazdirilanAlan();
+}
+});
