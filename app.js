a30cbe5b54c839528f8428e91260e2067cebe32c
Afra <nurafraciftci@gmail.com>
2023-06-13 14:52:51 +0200

    v1

---
+++ app.js
@@ -0,0 +1,5 @@
+const previous = document.querySelector(".previous-display");
+const current = document.querySelector(".current-display");
+const buttons = document.querySelector(".buttons-container");
+
+
